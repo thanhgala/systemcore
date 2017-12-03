a0ce8c1b975aaf47b34023c9e27011932cbd1fa3
DESKTOP-PMFMADO\Thanh <tabimtop@gmail.com>
2017-12-04 00:13:40 +0700

    complete base project asp.net core for every project

--- SystemCore.Data/Entities/ProductTag.cs
+++ SystemCore.Data/Entities/ProductTag.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+using SystemCore.Infrastructure.SharedKernel;
 
 namespace SystemCore.Data.Entities
 {
     [Table("ProductTags")]
-    public class ProductTag
+    public class ProductTag : DomainEntity<int>
     {
         public int ProductId { get; set; }
 
